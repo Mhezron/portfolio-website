8b1f63b2d7ba0fead8fa59cfc394803100928227
Mhezron <mhezron699@gmail.com>
2022-11-13 15:24:05 +0300

    updated links to social sites

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -99,7 +99,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='https://www.linkedin.com/in/mugeci-hezron-080139146/'
             >
               Linkedin <FaLinkedin size={30} />
             </a>
@@ -107,7 +107,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='https://github.com/Mhezron/Mhezron'
             >
               Github <FaGithub size={30} />
             </a>
@@ -115,7 +115,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='https://mail.google.com/mail/u/0/#inbox'
             >
               Email <HiOutlineMail size={30} />
             </a>
